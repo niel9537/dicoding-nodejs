3d5dccdd7eb6543865abf79bb93f122e59547e75
niel9537 <niel.zebua8@gmail.com>
2023-02-15 22:39:01 +0700

    upd:path and query done

--- hapi-web-server/route.js
+++ hapi-web-server/route.js
@@ -8,6 +8,18 @@ const route = [
   },
   {
     method: "GET",
+    path: "/hello/{name?}",
+    handler: (request, h) => {
+      const { name = "stranger" } = request.params;
+      const { lang } = request.query;
+      if (lang == "id") {
+        return `Hai ${name}!`;
+      }
+      return `Hello ${name}!`;
+    },
+  },
+  {
+    method: "GET",
     path: "/about",
     handler: (request, h) => {
       return "About Page";
